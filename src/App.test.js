1476fd356e056392e4a436cf4f599db5f95a8080
RasmusKlaaser <rasmus.klaaser@voco.ee>
2025-02-18 21:26:51 +0200

    header 2.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('renders weather application title', () => {
+    it('render weather application title', () => {  
+      render(<App />);
+      const linkElement = screen.getByText(/Weather Application/i);
+      expect(linkElement).toBeInTheDocument();
+  });
+})
+
